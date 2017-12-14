461e2ed7ee24f84a46c61e5fce874cab223ee6ac
matthewnc <mwg271@nyu.edu>
2017-12-14 05:22:36 -0500

    final

--- scripts/newMain.js
+++ scripts/newMain.js
@@ -1,11 +1,14 @@
-window.onload = showImages;
-
-
-function showImages(){
+function showImage1(){
 	var imgIndex1 = Math.round(Math.random() * 80);
-	var imgIndex2 = Math.round(Math.random() * 80);
-	var imgIndex3 = Math.round(Math.random() * 80);
 	document.getElementById("randomImage1").src = "usethese/random/" + imgIndex1 + ".jpg";
+}
+
+function showImage2(){
+	var imgIndex2 = Math.round(Math.random() * 80);
 	document.getElementById("randomImage2").src = "usethese/random/" + imgIndex2 + ".jpg";
+}
+
+function showImage3(){
+	var imgIndex3 = Math.round(Math.random() * 80);
 	document.getElementById("randomImage3").src = "usethese/random/" + imgIndex3 + ".jpg";
 }
